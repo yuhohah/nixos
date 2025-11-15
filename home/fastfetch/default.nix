b7b5adab0834e8d4291382b27e74f09096b13f89
yuhohah <luandepaulamota@hotmail.com>
2025-11-15 00:15:49 -0300

    Add logo.txt

--- home/fastfetch/default.nix
+++ home/fastfetch/default.nix
@@ -1,29 +1,149 @@
-home.file.".config/fastfetch/config.jsonc".text = ''
+{ config, pkgs, ... }:
+
 {
-  "logo": {
-    "type": "auto"
-  },
-  "display": {
-    "separator": " -> "
-  },
-  "modules": [
-    "title",
-    "separator",
-    "os",
-    "host",
-    "kernel",
-    "uptime",
-    "packages",
-    "shell",
-    "display",
-    "de",
-    "wm",
-    "terminal",
-    "cpu",
-    "gpu",
-    "memory",
-    "disk",
-    "colors"
-  ]
+programs.fastfetch = {
+    enable = true;
+    settings = {
+      "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+      logo = {
+        type = "file";
+        source = "./logo.txt";
+        color = {
+          "1" = "green";
+        };
+        padding = {
+          top = 2;
+          right = 6;
+          left = 2;
+        };
+      };
+      modules = [
+        "break"
+        {
+          type = "custom";
+          format = "\u001b[90m┌──────────────────────Hardware──────────────────────┐";
+        }
+        {
+          type = "host";
+          key = " PC";
+          keyColor = "green";
+        }
+        {
+          type = "cpu";
+          key = "│ ├";
+          showPeCoreCount = true;
+          keyColor = "green";
+        }
+        {
+          type = "gpu";
+          key = "│ ├";
+          detectionMethod = "pci";
+          keyColor = "green";
+        }
+        {
+          type = "display";
+          key = "│ ├󱄄";
+          keyColor = "green";
+        }
+        {
+          type = "disk";
+          key = "│ ├󰋊";
+          keyColor = "green";
+        }
+        {
+          type = "memory";
+          key = "│ ├";
+          keyColor = "green";
+        }
+        {
+          type = "swap";
+          key = "└ └󰓡 ";
+          keyColor = "green";
+        }
+        {
+          type = "custom";
+          format = "\u001b[90m└────────────────────────────────────────────────────┘";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "\u001b[90m┌──────────────────────Software──────────────────────┐";
+        }
+        {
+          type = "os";
+          key = " OS";
+          keyColor = "blue";
+        }
+        {
+          type = "kernel";
+          key = "│ ├";
+          keyColor = "blue";
+        }
+        {
+          type = "wm";
+          key = "│ ├";
+          keyColor = "blue";
+        }
+        {
+          type = "terminal";
+          key = "│ ├";
+          keyColor = "blue";
+        }
+        {
+          type = "shell";
+          key = "│ ├";
+          keyColor = "blue";
+        }
+        {
+          type = "packages";
+          key = "│ ├󰏖";
+          keyColor = "blue";
+        }
+        {
+          type = "wmtheme";
+          key = "│ ├󰉼";
+          keyColor = "blue";
+        }
+        {
+          type = "icons";
+          key = "│ ├󰀻";
+          keyColor = "blue";
+        }
+        {
+          type = "terminalfont";
+          key = "└ └";
+          keyColor = "blue";
+        }
+        {
+          type = "custom";
+          format = "\u001b[90m└────────────────────────────────────────────────────┘";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "\u001b[90m┌────────────────────Uptime / Age────────────────────┐";
+        }
+        {
+          type = "command";
+          key = "󱦟 OS Age";
+          keyColor = "magenta";
+          text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+        }
+        {
+          type = "uptime";
+          key = "󱫐 Uptime";
+          keyColor = "magenta";
+        }
+        {
+          type = "custom";
+          format = "\u001b[90m└────────────────────────────────────────────────────┘";
+        }
+        "break"
+        {
+          type = "colors";
+          symbol = "circle";
+        }
+      ];
+    };
+  };  
 }
-'';
