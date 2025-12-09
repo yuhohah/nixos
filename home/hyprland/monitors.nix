8e69623f594891bb2994c8f6c17fe43a588fa459
yuhohah <luandepaulamota@hotmail.com>
2025-12-09 19:19:38 -0300

    notebook waybar

--- home/hyprland/monitors.nix
+++ home/hyprland/monitors.nix
@@ -1,30 +1,41 @@
-{ config, pkgs, ... }:
+{ config, pkgs, osConfig, lib, ... }:
 
-{
-  wayland.windowManager.hyprland.settings = {
-    # Primeiro, descubra os nomes dos seus monitores
+let
+  hostname = osConfig.networking.hostName;
+
+  # Configuração Padrão (Desktop/PC)
+  desktopConfig = {
     monitor = [
-      # Formato: "Nome,Resolução,Posição,Scale"
-      
-      # Exemplo: Monitor primário
       "HDMI-A-1,1920x1080@144,0x0,1"
-      
-      # Exemplo: Monitor externo à direita
       "HDMI-A-2,1920x1080@74.973,1920x0,1"
-      
-      # Exemplo: Monitor externo acima
-      # "DP-1,1920x1080,0x-1080,1"
-      
-      # Exemplo: Com scale para HiDPI
-      # "eDP-1,2880x1800,0x0,2"
     ];
-    
-    # Workspaces por monitor (opcional)
     workspace = [
-      "1, monitor:HDMI-A-1"  # Workspace 1 no monitor primário
-      "2, monitor:HDMI-A-2"  # Workspace 2 no monitor externo
+      "1, monitor:HDMI-A-1"
+      "2, monitor:HDMI-A-2"
       "3, monitor:HDMI-A-1"
       "4, monitor:HDMI-A-2"
     ];
+  };
+
+  # Configuração do Notebook (Arrow)
+  notebookConfig = {
+    monitor = [
+      #,preferred,auto,1" # Auto-detectar com scale 1
+      "eDP-1, 1920x1080, 0x0, 1"
+    ];
+    workspace = [
+      "1, monitor:eDP-1"
+      "2, monitor:eDP-1"
+    ];
+  };
+
+  # Seleciona a config baseada no hostname
+  monitorConfig = if hostname == "arrow" then notebookConfig else desktopConfig;
+
+in
+{
+  wayland.windowManager.hyprland.settings = {
+    monitor = monitorConfig.monitor;
+    workspace = monitorConfig.workspace;
   };
 }
---
+++ home/waybar/notebook.nix
@@ -0,0 +1,123 @@
+{ config, pkgs, ... }:
+
+{
+  # Waybar Configuration for Notebook
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    style = ./style.css;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 24;
+        spacing = 4;
+        margin-top = 12;
+        margin-bottom = 0;
+        margin-left = 12;
+        margin-right = 12;
+
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "pulseaudio" "backlight" "battery" "network" "tray" ];
+
+        # Workspaces
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          format = "{icon}";
+          format-icons = {
+            "1" = "1";
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            "5" = "5";
+            "6" = "6";
+            "7" = "7";
+            "8" = "8";
+            "9" = "9";
+            "10" = "10";
+          };
+        };
+
+        # Clock
+        "clock" = {
+          format = "{:%H:%M:%S}";
+          format-alt = "{:%A, %d de %B de %Y}";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "month";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            on-click-right = "mode";
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+        };
+
+        # Audio
+        "pulseaudio" = {
+          format = "{icon} {volume}%";
+          format-muted = " MUTE";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = ["" "" ""];
+          };
+          on-click = "pavucontrol";
+          scroll-step = 5;
+        };
+
+        # Backlight
+        "backlight" = {
+          # device = "intel_backlight"; # Auto-detect by default
+          format = "{icon} {percent}%";
+          format-icons = ["" "" "" "" "" "" "" "" ""];
+          on-scroll-up = "brightnessctl set +5%";
+          on-scroll-down = "brightnessctl set 5%-";
+        };
+
+        # Battery
+        "battery" = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{icon} {capacity}%";
+          format-charging = " {capacity}%";
+          format-plugged = " {capacity}%";
+          format-alt = "{time} {icon}";
+          format-icons = ["" "" "" "" ""];
+        };
+
+        # Network
+        "network" = {
+          format-wifi = " {signalStrength}%";
+          format-ethernet = " {ifname}";
+          format-disconnected = " Disconnected";
+          tooltip-format = "{ifname} via {gwaddr}";
+          tooltip-format-wifi = "{essid} ({signalStrength}%)";
+          tooltip-format-ethernet = "{ifname} ";
+          tooltip-format-disconnected = "Disconnected";
+          max-length = 50;
+          on-click = "nm-connection-editor";
+        };
+
+        # Tray
+        "tray" = {
+          spacing = 10;
+        };
+      };
+    };
+  };
+}
