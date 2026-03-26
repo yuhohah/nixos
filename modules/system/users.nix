eb158be1921982d8da66781c214d879e52e6ed03
yuhohah <luandepaulamota@hotmail.com>
2026-03-26 14:56:25 -0300

    Optional guest included

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -9,7 +9,19 @@
     group = "luan";
     extraGroups = [ "wheel" "networkmanager" "audio" "video" "storage" "usb" ];
     home = "/home/luan";
+    homeMode = "700"; # Impede que outros usuários (como o convidado) acessem sua pasta
     packages = with pkgs; [ tree ];
   };
-}
 
+  # Guest User (Convidado)
+  #users.groups.convidado = {};
+
+  #users.users.convidado = {
+  #  isNormalUser = true;
+  #  description = "Convidado";
+  #  group = "convidado";
+  #  # Apenas grupos básicos, sem acesso root (wheel) ou dispositivos críticos
+  #  extraGroups = [ "networkmanager" "audio" "video" ];
+  #  home = "/home/convidado";
+  #};
+}
